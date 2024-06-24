ba0eac189eb861a44bb355e4fed33ec7f49f0316
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-06-24 14:05:49 +0200

    player one can make a choice

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 let handEl = document.getElementById("hand-el")
 let handElPlayer2 = document.getElementById("hand-elPlayer2")
+let rockEl = document.getElementById("rock-el")
+let paperEl = document.getElementById("paper-el")
+let scissorsEl = document.getElementById("scissors-el")
+let choiceEl = document.getElementById("choice-el")
+let hideEl = document.querySelector(".hide")
+
 let hands = ["Rock", "Paper", "Scissors"]
 
+rockEl.innerHTML = `<button style="border:none;background-color: #9B97A2;"><img alt=${hands[0]} src="./images/${hands[0]}.png"></button>`
+paperEl.innerHTML = `<button style="border:none;background-color: #9B97A2;"><img alt=${hands[1]} src="./images/${hands[1]}.png"></button>`
+scissorsEl.innerHTML = `<button style="border:none;background-color: #9B97A2;"><img alt=${hands[2]} src="./images/${hands[2]}.png"></button>`
+
+let selectedHand  
+
+
 //PLAYER 1
-function generateHand() {
-    let randomIndex = Math.floor(Math.random() * 3)
-    return hands[randomIndex]
+
+function startGame(){
+    hideEl.classList.remove("hide")
+    rockEl.addEventListener("click", () => {
+        selectedHand = hands[0];
+        showHand(selectedHand);
+    });
+    paperEl.addEventListener("click", () => {
+        selectedHand = hands[1];
+        showHand(selectedHand);
+    });
+    scissorsEl.addEventListener("click", () => {
+        selectedHand = hands[2];
+        showHand(selectedHand);
+    });
 }
 
-
-function showHand(){
-    showHandPlayer2()
-    let generatedImg = generateHand()
+function showHand(selectedHand){
+    choiceEl.classList.add("hide")
+    let generatedImg = selectedHand
     handEl.innerHTML = `<img alt=${generatedImg} src="./images/${generatedImg}.png">`
+    console.log (generatedImg)
+    showHandPlayer2()
 }
 
 
